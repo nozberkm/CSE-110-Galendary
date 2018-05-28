30ee98e5eec89fcc39fd6709eb276b9c2fb69dbf
Chaitanya <c2patel@ucsd.edu>
2018-05-28 14:53:47 -0700

    Trying to Login

--- app/src/main/java/com/example/aymaan/cse110applogin/LoginActivity.java
+++ app/src/main/java/com/example/aymaan/cse110applogin/LoginActivity.java
@@ -2,11 +2,14 @@ package com.example.aymaan.cse110applogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.content.Intent;
 import android.widget.TextView;
 import android.view.View;
 import android.support.v7.widget.CardView;
+
+import com.example.jeff.database_access.UserObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,11 +18,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        
         final EditText etLoginEmail = (EditText) findViewById(R.id.etLoginEmail);
         final EditText etLoginPassword = (EditText) findViewById(R.id.etLoginPassword);
         final TextView tLoginRecovery = (TextView) findViewById(R.id.tLoginRecovery);
         final CardView cLoginLogin = (CardView) findViewById(R.id.cLoginLogin);
         final CardView cLoginRegister = (CardView) findViewById(R.id.cLoginRegister);
+        final EditText etLoginError = (EditText) findViewById(R.id.etLoginError);
+
 
 
         cLoginRegister.setOnClickListener(new View.OnClickListener(){
@@ -41,8 +47,28 @@ public class LoginActivity extends AppCompatActivity {
         cLoginLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent cLoginLoginIntent = new Intent(LoginActivity.this, SuccessActivity.class);
-                LoginActivity.this.startActivity(cLoginLoginIntent);
+                String username = etLoginEmail.getText().toString();
+                String password = null;
+                try {
+                    password = Hashing.SHA1(etLoginPassword.getText().toString());
+                }
+                catch (Exception e) {
+                    Log.e("SHA1",e.getMessage());
+                }
+
+                UserObject user = new UserObject(username, password);
+
+                if(user.fetchFromDatabase() == null){
+                    //login failed
+                    etLoginError.setText("Login Failure");
+                } else {
+                    //login success
+                    Intent cLoginLoginIntent = new Intent(LoginActivity.this, SuccessActivity.class);
+                    LoginActivity.this.startActivity(cLoginLoginIntent);
+                }
+
+
+
             }
         });
 
