38f26b886136749754890fe902553359cc8c13b0
cmshroe <cmschroe@ucsd.edu>
2018-05-31 14:56:45 -0700

    group search not on join group page

--- app/src/main/java/com/example/aymaan/cse110applogin/JoinGroup.java
+++ app/src/main/java/com/example/aymaan/cse110applogin/JoinGroup.java
@@ -2,6 +2,7 @@ package com.example.aymaan.cse110applogin;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 
@@ -16,41 +18,27 @@ public class JoinGroup extends AppCompatActivity {
 
 
     Context context;
+    private Button search;
+    private Button join;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_group);
         context=this;
-        ListView lstview=(ListView)findViewById(R.id.listv);
-        ArrayAdapter adapter;
+        join = (Button) findViewById(R.id.button);
+        search = (Button) findViewById(R.id.button2);
 
-        final String[] items={"Random Group 1","Random Group 2","Random Group 3","Random Group 4","Random Group 5"};
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
-        lstview.setAdapter(adapter);
-        lstview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        join.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                AlertDialog.Builder builder;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
-                } else {
-                    builder = new AlertDialog.Builder(context);
-                }
-                builder.setTitle("Join Group " + items[position])
-                        .setMessage("Are you sure you want to join this group")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // continue with join
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
-                            }
-                        })
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
+            public void onClick(View v) {
+
             }
         });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
 }
