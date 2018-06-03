2e47eb524a3305b3b750304c4352c01c4dc6b6aa
Aymaan Ashhab Ahmed <aymaan@Aymaans-MacBook-Air.local>
2018-06-03 14:56:53 -0700

    tab addevents

---
+++ app/src/main/java/com/example/aymaan/cse110applogin/AddEventFragment.java
@@ -0,0 +1,21 @@
+package com.example.aymaan.cse110applogin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddEventFragment extends Fragment {
+    private static final String TAG = "AddEventFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_event_fragment, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/aymaan/cse110applogin/AddTaskEventActivity.java
@@ -0,0 +1,48 @@
+package com.example.aymaan.cse110applogin;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class AddTaskEventActivity extends AppCompatActivity {
+
+    private static final String TAG = "AddTaskEventFragment";
+
+    private SectionsPageAdapter mSectionPageAdapter;
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task_event);
+
+        mSectionPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AddEventFragment(),"Add Event");
+        adapter.addFragment(new AddTaskFragment(),"Add Task");
+        viewPager.setAdapter(adapter);
+    }
+}
