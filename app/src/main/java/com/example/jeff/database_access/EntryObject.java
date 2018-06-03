7f094aa343262decf06d2970a2b6dd32287bcd28
Nabi Can Ozberkman <nabicanozberkman@Nabis-MacBook-Pro.local>
2018-06-02 23:33:00 -0700

    Merge

--- app/src/main/java/com/example/jeff/database_access/EntryObject.java
+++ app/src/main/java/com/example/jeff/database_access/EntryObject.java
@@ -214,13 +214,6 @@ public class EntryObject {
         return getDayString(getStart());
     }
 
-    public boolean isTask() {
-        if (this.getStart() == null)
-            return true;
-        else
-            return false;
-    }
-
     public static String getDayOfWeek(Date date) {
         return new SimpleDateFormat("EEE").format(date);
     }
