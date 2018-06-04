46f86023d052b631b90434efa9d1efd1941408ec
Jeffrey Cash <jtcash@ucsd.edu>
2018-06-03 17:29:48 -0700

    PP Problem: Fixed null handling issue

--- app/src/main/java/com/example/aymaan/cse110applogin/Home.java
+++ app/src/main/java/com/example/aymaan/cse110applogin/Home.java
@@ -92,6 +92,7 @@ public class Home extends AppCompatActivity {
             //toolbar.setTitle("Man");
             for(String s: EntryMap.keySet()) {
                 Date date = EntryObject.getDayDateFromString(s);
+                if(date == null) continue;
                 for(int i=0; i<EntryMap.get(s).size(); i++) {
                     Event ev1 = new Event(Color.BLACK, date.getTime());
                     compactCalendarView.addEvent(ev1);
