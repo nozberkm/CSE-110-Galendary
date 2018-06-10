142d14223cdb871046aad11f070219199b7bad4f
Jeffrey Cash <jtcash@ucsd.edu>
2018-06-09 18:52:31 -0700

    Continued cleanup

--- app/src/main/java/com/example/jeff/database_access/UserObject.java
+++ app/src/main/java/com/example/jeff/database_access/UserObject.java
@@ -321,10 +321,6 @@ public class UserObject {
     }
 
 
-    // Search for groups by name
-    public ArrayList<GroupObject> getGroupsMatchingString(String groupName){
-        return DatabaseRequest.search_group_name(groupName);
-    }
 
     public boolean create_request(long group_id) throws IOException {
         return DatabaseRequest.create_request(this,group_id);
@@ -342,24 +338,15 @@ public class UserObject {
     public ArrayList<GroupRequestObject> get_requests() {
         return DatabaseRequest.get_requests(this);
     }
-    //TODO fix the groupRequestObject
 
 
 
-
-
-
-
-
-
+    // Change a User's password
     public boolean changePassword(String current_pass_plaintext, String new_pass_plaintext){
         if(getId() < 0 || getUsername() == null) return false;
         if(current_pass_plaintext == null || new_pass_plaintext == null) return false;
 
-
         String current_passhash = null;
-
-
         String new_passhash = null;
 
         try {
@@ -374,7 +361,6 @@ public class UserObject {
         }
 
         if(current_passhash == null || new_passhash == null) return false;
-
 
         boolean status = false;
 
@@ -391,29 +377,8 @@ public class UserObject {
     }
 
 
-
-
-
-
-
-
-    public static boolean ResetPassword(String password){
-        boolean status = false;
-
-        try {
-            status = DatabaseRequest.reset_password(password);
-        } catch (IOException e) {
-            e.printStackTrace();
-            status = false;
-        } catch (JSONException e) {
-            e.printStackTrace();
-            status = false;
-        }
-        return status;
-    }
-
-
-
+    // Permanently delete this UserObject, locally and in the database
+    // Be careful
     public boolean delete(){
         boolean success = false;
 
@@ -425,6 +390,7 @@ public class UserObject {
             e.printStackTrace();
         }
 
+        // This has been deleted from the database. Reflect changes locally
         if(success){
             // Make the garbage collector a happiboi
             id = -1;
@@ -444,44 +410,27 @@ public class UserObject {
     }
 
 
+    // Static method to allow password reset email functionality to occur
+    public static boolean ResetPassword(String password){
+        boolean status = false;
+
+        try {
+            status = DatabaseRequest.reset_password(password);
+        } catch (IOException e) {
+            e.printStackTrace();
+            status = false;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            status = false;
+        }
+        return status;
+    }
 
 
+    // Search for groups by name
+    public static ArrayList<GroupObject> getGroupsMatchingString(String groupName){
+        return DatabaseRequest.search_group_name(groupName);
+    }
 
 
-
-
-//    private boolean updateDatabase(){
-//
-//
-//        return true;
-//    }
-
-//    private boolean assign(UserObject other){
-//        if(other == null) return false;
-////        if(username != other.getUsername()) return false;
-//        id = other.getId();
-//        username = other.getUsername();
-//        passhash = other.getPasshash();
-//        email_confirmed = other.isEmailConfirmed();
-//        notifications = other.isNotifications();
-//        name = other.getName();
-//        exists = other.exists;
-//        return true;
-//    }
-//
-//    public boolean synchronizeWithDatabase(){
-//        UserObject dbUserObject = DatabaseRequest.get_user(getUsername(), getPasshash());
-//        if(dbUserObject == null) return false;
-//
-//        if(this.compareTo(dbUserObject) != 0){
-//            return this.assign(dbUserObject);
-//        }
-//
-//        return false;
-//    }
-//
-//    public int compareTo(UserObject other){
-//        if(other == null) return Integer.MIN_VALUE;
-//        return toString().compareTo(other.toString());
-//    }
 }
