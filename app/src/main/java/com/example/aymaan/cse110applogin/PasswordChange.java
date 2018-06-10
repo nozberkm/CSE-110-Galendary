cfe96fc1d42122008ec3929e4369255f97883b75
Jor-el Briones <Jor-el@Jor-els-MacBook-Pro.local>
2018-06-09 18:31:51 -0700

    addded logic for checking inputs. This was here prior to me switching tasks

--- app/src/main/java/com/example/aymaan/cse110applogin/PasswordChange.java
+++ app/src/main/java/com/example/aymaan/cse110applogin/PasswordChange.java
@@ -1,5 +1,7 @@
 package com.example.aymaan.cse110applogin;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,16 +11,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordChange extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password_change);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Password Change");
+        setContentView(R.layout.activity_change_password);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(PasswordChange.this);
+
+
+        //mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.nav_action);
+        //setSupportActionBar(mToolbar);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
 
         final Button changePass = (Button) findViewById(R.id.changePass);
         final TextView currentPassField = (TextView) findViewById(R.id.currentPassImp);
@@ -31,13 +45,48 @@ public class PasswordChange extends AppCompatActivity {
                 String currentPass = currentPassField.getText().toString();
                 String newPass = newPassField.getText().toString();
                 String reenterNewPass = reenterNewPassField.getText().toString();
-                if(newPass.equals(reenterNewPass)&&currentPass.equals(reenterNewPass)) {
-                    //  LoginActivity.userLogin.
-                    changePass.setBackgroundColor(100);
-                    finish();
+
+                try {
+                    currentPass = Hashing.SHA1(currentPass);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
                 }
+
+                //Check if valid
+                if(currentPass == null || currentPass == "" || newPass == null || newPass == "" ||
+                        reenterNewPass == null || reenterNewPass == ""){
+                    builder.setMessage("Please fill in all fields.");
+                    Dialog fillError = builder.create();
+                }
+                else if(LoginActivity.userLogin.getPasshash() != currentPass){
+                    builder.setMessage("Current password Incorrect.");
+                    Dialog verifyError = builder.create();
+                }
+                else if(newPass != reenterNewPass) {
+                    builder.setMessage("New password does not match.");
+                    Dialog matchError = builder.create();
+                }
+                else{
+                    try {
+                        newPass = Hashing.SHA1(newPass);
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    //Update password with new hash
+                }
+
+
+
+                changePass.setBackgroundColor(100);
+                finish();
             }
         });
+
+
 
 
 
